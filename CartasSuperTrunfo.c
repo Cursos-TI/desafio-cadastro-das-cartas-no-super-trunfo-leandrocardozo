d2e8def2cf61a463289109f41e54833adffb905d
Leandro C Braga <98498196+leandrocardozo@users.noreply.github.com>
2025-04-16 19:45:55 +0000

    Inicializando variaveis do jogo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Aluno: Leandro Cardoso Braga
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    char nomeCidade_1[20];
+    int codigoCidade_1;
+    int ptsTuristicosCidade_1;
+    float populacaoCidade_1;
+    float areaCidade_1;
+    float pibCidade_1;
+
     
-    // Cadastro das Cartas:
+    printf("==================================\n");
+    printf(">>> JOGO DE CARTA SUPER TRUNFO <<<\n");
+    printf("------- Cadastro de cartas -------\n");
+    printf("==================================\n");
+    printf("* Carta número 1 *\n\n");
+
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    printf("Digite o n´");
+
+
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
